b8b8a6f297536f03c0674734e8df3e649d4d27d9
Sandra Potet <potetsandra44@gmail.com>
2026-05-06 18:31:53 +0200

    Add book to user library

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -20,3 +20,50 @@ export const getMe = (req, res) => {
     },
   );
 };
+
+// Ajouter un livre à sa bibliothèque perso
+export const addBookToMyLibrary = (req, res) => {
+  const userId = req.user.userId;
+  const { bookId } = req.body;
+
+  if (!bookId) {
+    return res.status(400).json({
+      error: "Veuillez choisir un livre à ajouter à votre bibliothèque",
+    });
+  }
+
+  db.get(`SELECT id FROM books WHERE id = ?`, [bookId], (err, existingBook) => {
+    if (err) {
+      return res.status(500).json({ error: "Erreur serveur" });
+    }
+
+    if (!existingBook) {
+      return res.status(404).json({ error: "Livre introuvable" });
+    }
+
+    db.run(
+      `INSERT INTO user_books (user_id, book_id, status) VALUES (?, ?, ?)`,
+      [userId, bookId, "to_read"],
+      function (err) {
+        if (err) {
+          if (err.message.includes("UNIQUE constraint failed")) {
+            return res
+              .status(409)
+              .json({ error: "Ce livre est déjà dans votre bibliothèque" });
+          }
+
+          return res.status(500).json({ error: "Erreur serveur" });
+        }
+
+        res.status(201).json({
+          id: this.lastID,
+          userId,
+          bookId,
+          status: "to_read",
+          recommendation: null,
+          comment: null,
+        });
+      },
+    );
+  });
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMe } from "../controllers/users.controller.js";
+import { getMe, addBookToMyLibrary } from "../controllers/users.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/me", authMiddleware, getMe);
+router.post("/me/books", authMiddleware, addBookToMyLibrary);
 
 export default router;
