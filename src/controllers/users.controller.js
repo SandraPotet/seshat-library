1d19ee72a72ab774c74d1ab6bb61b4f0f2d6e2bc
Sandra Potet <potetsandra44@gmail.com>
2026-05-06 17:50:10 +0200

    Return current user profile

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,22 @@
+import db from "../models/db.js";
+
 // voir la page de l'utilisateur connecté
 export const getMe = (req, res) => {
-  res.json(req.user);
+  const userId = req.user.userId;
+
+  db.get(
+    `SELECT id, username FROM users WHERE id = ?`,
+    [userId],
+    (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: "Erreur serveur" });
+      }
+
+      if (!user) {
+        return res.status(404).json({ error: "Utilisateur introuvable" });
+      }
+
+      res.json(user);
+    },
+  );
 };
